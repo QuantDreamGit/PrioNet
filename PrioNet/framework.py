30e1862e2f4961193577f8ce114690dae456ea28
Prio124 <GIUSEppe11@@>
2024-05-27 09:43:49 +0200

    added minimum quantity check

--- PrioNet/framework.py
+++ PrioNet/framework.py
@@ -4,7 +4,11 @@ from .telegram import Telegram
 
 
 class Framework:
-    def __init__(self, bot_name, telegram_api_key, telegram_chat_id, bybit_api_key, bybit_api_secret, quiet_mode=False, testnet=False):
+    def __init__(self, 
+                 bot_name, 
+                 telegram_api_key, telegram_chat_id, 
+                 bybit_api_key, bybit_api_secret, 
+                 quiet_mode=False, testnet=False):
         # Set the bot name
         self.bot_name = bot_name
         # Set the quiet mode of the bot
@@ -44,6 +48,16 @@ class Framework:
         self.qty = qty
         self.tp = tp
         self.sl = sl
+
+        # Get the minimum quantity for the symbol
+        min_qty = self.bybit.get_precision(symbol)
+
+        # Check if the quantity is higher than the minimum quantity
+        if qty < min_qty:
+            # Send a message to the Telegram chat
+            self.telegram.send_message(f'{self.bot_name} quantity is lower than the minimum quantity for {symbol}.')
+            # Return the error message
+            return f'Quantity is lower than the minimum quantity for {symbol}.'
 
         # Set the information for the ByBit object
         self.bybit.set_info(symbol, qty, tp, sl)
@@ -97,62 +111,58 @@ class Framework:
         #  Now, verify if current position should be modified
         # Long entry signal
         if entry_mask and not short_entry_mask:
+            # Send a message to the Telegram chat
+            self.telegram.send_message(f'{self.bot_name} opened a LONG position for {self.symbol}.')
             # Open a long position
             order = self.bybit.market_order('Buy')
             # Save the status
             self.set_status('LONG')
-            # Send a message to the Telegram chat
-            self.telegram.send_message(f'{self.bot_name} opened a LONG position for {self.symbol}.')
+            
         # Long exit signal
         if exit_mask and not short_exit_mask:
+            # Send a message to the Telegram chat
+            self.telegram.send_message(f'{self.bot_name} closed the LONG position for {self.symbol}.')
             # Close the long position
             order = self.bybit.market_order('Sell')
             # Save the status
             self.set_status('NEUTRAL')
-            # Send a message to the Telegram chat
-            self.telegram.send_message(f'{self.bot_name} closed the LONG position for {self.symbol}.')
+            
         # Short entry signal
         if short_entry_mask and not entry_mask:
+            # Send a message to the Telegram chat
+            self.telegram.send_message(f'{self.bot_name} opened a SHORT position for {self.symbol}.')
             # Open a short position
             order = self.bybit.market_order('Sell')
             # Save the status
-            self.set_status('SHORT')
-            # Send a message to the Telegram chat
-            self.telegram.send_message(f'{self.bot_name} opened a SHORT position for {self.symbol}.')
+            self.set_status('SHORT')        
 
         # Short exit signal
         if short_exit_mask and not entry_mask:
+            # Send a message to the Telegram chat
+            self.telegram.send_message(f'{self.bot_name} closed the SHORT position for {self.symbol}.')
             # Close the short position
             order = self.bybit.market_order('Buy')
             # Save the status
             self.set_status('NEUTRAL')
-            # Send a message to the Telegram chat
-            self.telegram.send_message(f'{self.bot_name} closed the SHORT position for {self.symbol}.')
-        
-        ''' TODO: Implement the reverse position signals 
+            
         # Reverse position signal
         if reverse_mask:
+            # Send a message to the Telegram chat
+            self.telegram.send_message(f'{self.bot_name} reversed the LONG position for {self.symbol} to SHORT.')
             # Close the long position
             order = self.bybit.market_order('Sell')
             # Open a short position
             order = self.bybit.market_order('Sell')
             # Save the status
             self.set_status('SHORT')
-            # Send a message to the Telegram chat
-            self.telegram.send_message(f'{self.bot_name} reversed the LONG position for {self.symbol} to SHORT.')
-
+            
         # Short reverse position signal
         if short_reverse_mask:
+            # Send a message to the Telegram chat
+            self.telegram.send_message(f'{self.bot_name} reversed the SHORT position for {self.symbol} to LONG.')
             # Close the short position
             order = self.bybit.market_order('Buy')
             # Open a long position
             order = self.bybit.market_order('Buy')
             # Save the status
             self.set_status('LONG')
-            # Send a message to the Telegram chat
-            self.telegram.send_message(f'{self.bot_name} reversed the SHORT position for {self.symbol} to LONG.')
-        '''
-
-        
-    
-
